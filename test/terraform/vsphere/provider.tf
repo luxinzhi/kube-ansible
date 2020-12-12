c791641ebc508560255c9ed8bcbd3eca2af2715f
buxiaomo <95112082@qq.com>
2020-12-12 17:15:52 +0800

    update terraform and add jenkinsfile

--- test/terraform/vsphere/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "vsphere" {
-  version              = "~> 1.12"
-  user                 = var.vsphere_user
-  password             = var.vsphere_password
-  vsphere_server       = var.vsphere_server
-  allow_unverified_ssl = true
-}
