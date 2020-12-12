adaf49d82c94ee991a6543d5c2ce20ca596abfab
buxiaomo <95112082@qq.com>
2020-12-12 20:09:57 +0800

    add backend

--- test/terraform/vsphere/main.tf
+++ test/terraform/vsphere/main.tf
@@ -1,17 +1,17 @@
 #===============================================================================
 # vSphere Backend
 #===============================================================================
-# terraform {
-#   backend "s3" {
-#     region                      = "minio"
-#     force_path_style            = true
-#     skip_credentials_validation = true
-#     skip_metadata_api_check     = true
-#     skip_region_validation      = true
-#     # skip_requesting_account_id  = true
-#     # skip_get_ec2_platforms      = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    region                      = "minio"
+    force_path_style            = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    # skip_requesting_account_id  = true
+    # skip_get_ec2_platforms      = true
+  }
+}
 
 
 #===============================================================================
